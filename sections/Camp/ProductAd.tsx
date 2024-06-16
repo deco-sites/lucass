228e8dff2c25bc9cf59cc1fafd518a7260de06f9
lucastakwe <lucas.santos@wedigi.com.br>
2024-06-15 21:56:57 -0300

    feat: adiciona error fallback e loading

--- sections/Camp/ProductAd.tsx
+++ sections/Camp/ProductAd.tsx
@@ -1,1 +1,26 @@
 export { default, loader } from "../../components/camp/ProductAd.tsx";
+
+export function ErrorFallback() {
+    return (
+        <div class="container px-1 my-2">
+            <p class="text-red-700 bold">Ocorreu um erro! Tente novamente mais tarde! </p>
+        </div>
+    );
+}
+  
+export function LoadingFallback() {
+    return (
+        <div class="container px-2">
+            <div class="flex w-fit flex-col sm:flex-row gap-2 p-2 rounded my-2 min-h-[375px] max-h-[375px]">
+                <div 
+                    class="skeleton block min-w-[300px] w-[300px] h-[300px]"
+                />
+                <div class="flex flex-col gap-2 items-start w-full mt-2">
+                    <div class="skeleton h-2 w-44"></div>
+                    <div class="skeleton h-2 w-full"></div>
+                    <div class="skeleton h-2 w-full"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
